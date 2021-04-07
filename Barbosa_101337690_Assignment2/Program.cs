d1686fcf70b3a975159552a6f5e4f32dad2af5d1
otaviobarbosa617 <59937699+otaviobarbosa617@users.noreply.github.com>
2021-04-07 17:01:49 -0400

    Method creation instead of giant loop.

--- Barbosa_101337690_Assignment2/Program.cs
+++ Barbosa_101337690_Assignment2/Program.cs
@@ -71,10 +71,9 @@ namespace Barbosa_101337690_Assignment2
             int birthMonth;
             int birthYear;
             int contactsSize = 100;
-            int limit = 0;
-            Contacts[] ct = new Contacts[contactsSize];
+            int counter = 0;
+            Contacts[] contact = new Contacts[contactsSize];
             int userChoice = 0;
-            string val;
 
             //menu loop
             while (userChoice != 5)
@@ -90,7 +89,7 @@ namespace Barbosa_101337690_Assignment2
                 Console.WriteLine("5) Exit");
                 Console.WriteLine();
                 Console.Write("Your choice from 1 to 5: ");
-                userChoice = Convert.ToInt32(Console.ReadLine());
+                userChoice = int.Parse(Console.ReadLine());
                 Console.WriteLine();
 
                 //menu selections
@@ -98,31 +97,16 @@ namespace Barbosa_101337690_Assignment2
                 switch (userChoice)
                 {
                     case 1:
-                        if (limit < contactsSize)
+                        if (counter < contactsSize)
                         {
-                            Console.WriteLine("Enter all information for New Contact ");
-                            Console.WriteLine();
-                            Console.Write("First Name: ");
-                            firstName = Console.ReadLine();
-                            Console.Write("Last Name: ");
-                            lastName = Console.ReadLine();
-                            Console.Write("E-mail address: ");
-                            email = Console.ReadLine();
-                            Console.Write("Phone Number: ");
-                            phoneNumber = Console.ReadLine();
-                            Console.Write("Day of Birth: ");
-                            birthDay = Convert.ToInt32(Console.ReadLine());
-                            Console.Write("Month of Birth: ");
-                            birthMonth = Convert.ToInt32(Console.ReadLine());
-                            Console.Write("Year of Birth: ");
-                            birthYear = Convert.ToInt32(Console.ReadLine());
-                            Console.WriteLine();
-                            ct[limit] = new Contacts(firstName, lastName, email, phoneNumber, birthDay, birthMonth, birthYear);
-                            limit++;
-                            Console.WriteLine("Contact added to the list");
-                            Console.WriteLine();
-                            Console.WriteLine("Press enter to return to menu");
-                            Console.ReadKey();
+                            string addNew;
+                            do
+                            {
+                                addContact();
+                                addNew = Console.ReadLine();
+                                Console.WriteLine();
+
+                            } while (addNew == "yes" || addNew == "Y" || addNew == "y");
                         }
                         else
                         {
@@ -133,7 +117,7 @@ namespace Barbosa_101337690_Assignment2
                         }
                         break;
                     case 2:
-                        if (limit == 0)
+                        if (counter == 0)
                         {
                             Console.WriteLine("ALERT: The contact list is empty");
                             Console.WriteLine();
@@ -142,23 +126,88 @@ namespace Barbosa_101337690_Assignment2
                         }
                         else
                         {
-                            Console.WriteLine("The full contact list is:");
-                            for (int i = 0; i < limit; i++)
-                            {
-                                Console.WriteLine($"\nContact {i}) {ct[i].GetFullName()}");
-                            }
-                            Console.WriteLine("Press enter to return to menu");
-                            Console.ReadKey();
+                            displayContactList();
                         }
                         break;
+                    case 3:
+                        findContact();
+                        break;
                     default:
+                        Console.WriteLine();
+                        Console.WriteLine("Invalid selection! Try again!");
+                        Console.WriteLine();
                         break;
                 }
             }
 
+            void addContact()
+            {
+                Console.WriteLine("Enter all information for New Contact ");
+                Console.WriteLine();
+                Console.Write("First Name: ");
+                firstName = Console.ReadLine();
+                Console.Write("Last Name: ");
+                lastName = Console.ReadLine();
+                Console.Write("E-mail address: ");
+                email = Console.ReadLine();
+                Console.Write("Phone Number: ");
+                phoneNumber = Console.ReadLine();
+                Console.Write("Day of Birth: ");
+                birthDay = int.Parse(Console.ReadLine());
+                Console.Write("Month of Birth: ");
+                birthMonth = int.Parse(Console.ReadLine());
+                Console.Write("Year of Birth: ");
+                birthYear = int.Parse(Console.ReadLine());
+                Console.WriteLine();
+                contact[counter] = new Contacts(firstName, lastName, email, phoneNumber, birthDay, birthMonth, birthYear);
+                counter++;
+                Console.WriteLine("Contact added to the list!");
+                Console.WriteLine();
+                Console.WriteLine("Do you wish to continue?");
+                Console.WriteLine("Y or Yes to continue, any other key to stop");
+            }
 
+            void displayContactList()
+            {
+                Console.WriteLine("The full contact list is:");
+                for (int i = 0; i < counter; i++)
+                {
+                    Console.WriteLine($"\nContact #{i + 1} - {contact[i].GetFullName()}");
+                }
+                Console.WriteLine();
+                Console.WriteLine("Press enter to return to menu");
+                Console.ReadKey();
+            }
 
-            // Contacts allContacts = new Contacts(firstName, lastName, email, phoneNumber, birthDay, birthMonth, birthYear);
+            void findContact()
+            {
+                Console.WriteLine("Contact finder - Enter the information");
+                Console.WriteLine();
+                Console.Write("First Name: ");
+                firstName = Console.ReadLine();
+                Console.Write("Last Name: ");
+                lastName = Console.ReadLine();
+                for (int i = 0; i < counter; i++)
+                {
+                    // You got to love Microsoft documentation:
+                    if (contact[i].GetFirstName().Equals(firstName, StringComparison.OrdinalIgnoreCase) && (contact[i].GetLasttName().Equals(lastName, StringComparison.OrdinalIgnoreCase)))
+                    {
+                        Console.WriteLine("\nContact found: \n");
+                        Console.WriteLine($"{contact[i].ToInfo()}");
+                        Console.WriteLine();
+                        Console.WriteLine("Press enter to return to menu");
+                        Console.ReadKey();
+                    }
+                    else
+                    {
+                        Console.WriteLine("\nContact NOT found: \n");
+                        Console.WriteLine();
+                        Console.WriteLine("Press enter to return to menu");
+                        Console.ReadKey();
+                    }
+                }
+
+            }
 
         }
     }
