3ab798b121d725b122b41312849e6afb707d93eb
otaviobarbosa617 <59937699+otaviobarbosa617@users.noreply.github.com>
2021-04-07 08:15:39 -0400

    first

---
+++ Barbosa_101337690_Assignment2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Barbosa_101337690_Assignment2
+{
+    class Contacts
+    {
+        //declarations
+        private string firstName;
+        private string lastName;
+        private string phone;
+        private string email;
+        private int dayOfBirth;
+        private int monthOfBirth;
+        private int yearOfBirth;
+
+    }
+
+
+    class MainProgram
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Wod!");
+        }
+    }
+}
