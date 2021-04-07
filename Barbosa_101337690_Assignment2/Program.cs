06d5285e985957ffb7ce438feb7e4444655410da
otaviobarbosa617 <59937699+otaviobarbosa617@users.noreply.github.com>
2021-04-07 08:38:55 -0400

    First object creation

--- Barbosa_101337690_Assignment2/Program.cs
+++ Barbosa_101337690_Assignment2/Program.cs
@@ -13,6 +13,48 @@ namespace Barbosa_101337690_Assignment2
         private int monthOfBirth;
         private int yearOfBirth;
 
+        //Object
+        public Contacts(string firstName, string lastName, string phone, string email, int dayOfBirth, int monthOfBirth, int yearOfBirth)
+        {
+            this.firstName = firstName ?? throw new ArgumentNullException(nameof(firstName));
+            this.lastName = lastName ?? throw new ArgumentNullException(nameof(lastName));
+            this.phone = phone ?? throw new ArgumentNullException(nameof(phone));
+            this.email = email ?? throw new ArgumentNullException(nameof(email));
+            this.dayOfBirth = dayOfBirth;
+            this.monthOfBirth = monthOfBirth;
+            this.yearOfBirth = yearOfBirth;
+        }
+
+        //all names had to be first letter capitalized unless I would get a warning. So, I had to digress from the UML
+        public string GetFirstName()
+        {
+            return firstName;
+        }
+
+        public string GetLasttName()
+        {
+            return firstName;
+        }
+
+        public string GetFullName()
+        {
+            return firstName + " " + lastName;
+        }
+
+        public string GetPhone()
+        {
+            return phone;
+        }
+
+        public string GetDateOfBirth()
+        {
+            return dayOfBirth + "-" + monthOfBirth + "-" + yearOfBirth;
+        }
+
+        public string ToInfo()
+        {
+            return $"Name: {GetFullName()}\nPhone Number: {GetPhone()}\nEmail: {email}\nDate of Birth: {GetDateOfBirth()}\n";
+        }
     }
 
 
